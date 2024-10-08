e6eb14019513a90b9adb54676829468c71244307
Zayartun552 <40667678@live.napier.ac.uk>
2024-10-09 01:04:33 +0630

    Country query table output

--- src/main/java/com/napier/devops/App.java
+++ src/main/java/com/napier/devops/App.java
@@ -1,9 +1,12 @@
 package com.napier.devops;
 
-import java.sql.*;
+import com.mysql.cj.xdevapi.Table;
 
-public class App
-{
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
     /**
      * Connection to MySQL database.
      */
@@ -12,39 +15,29 @@ public class App
     /**
      * Connect to the MySQL database.
      */
-    public void connect()
-    {
-        try
-        {
+    public void connect() {
+        try {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
         int retries = 10;
-        for (int i = 0; i < retries; ++i)
-        {
+        for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
-            try
-            {
+            try {
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false&allowPublicKeyRetrieval=true", "root", "group-5");
                 System.out.println("Successfully connected");
                 break;
-            }
-            catch (SQLException sqle)
-            {
+            } catch (SQLException sqle) {
                 System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
+            } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
@@ -53,82 +46,111 @@ public class App
     /**
      * Disconnect from the MySQL database.
      */
-    public void disconnect()
-    {
-        if (con != null)
-        {
-            try
-            {
+    public void disconnect() {
+        if (con != null) {
+            try {
                 // Close connection
                 con.close();
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 System.out.println("Error closing connection to database");
             }
         }
     }
 
-    public Employee getEmployee(int ID)
-    {
-        try
-        {
-            // Create an SQL statement
+    public static List<Country> getPopulatedCountries(Connection con, String key, String value, int limit) {
+        String Country_query = "SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.Name AS Capital " +
+                "FROM country JOIN city ON country.Capital = city.ID ";
+
+        if (key != null && value != null) {
+            Country_query += "WHERE country." + key + " = '" + value + "' ";
+        }
+
+        // Ensure a positive limit value for the query
+        if (limit > 0) {
+            Country_query += "ORDER BY country.Population DESC LIMIT " + limit;
+        } else {
+            Country_query += "ORDER BY country.Population DESC"; // No limit if zero or negative
+        }
+
+
+        return getCountryList(con, Country_query);
+    }
+
+    // Helper function to execute the query and return a list of countries
+    private static List<Country> getCountryList(Connection con, String Country_query) {
+        List<Country> countries = new ArrayList<>();
+        try {
             Statement stmt = con.createStatement();
-            // Create string for SQL statement
-            String strSelect =
-                    "SELECT emp_no, first_name, last_name "
-                            + "FROM employees "
-                            + "WHERE emp_no = " + ID;
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
-            // Return new employee if valid.
-            // Check one is returned
-            if (rset.next())
-            {
-                Employee emp = new Employee();
-                emp.emp_no = rset.getInt("emp_no");
-                emp.first_name = rset.getString("first_name");
-                emp.last_name = rset.getString("last_name");
-                return emp;
+            ResultSet rset = stmt.executeQuery(Country_query);
+
+            // Populate country list
+            while (rset.next()) {
+                String countryCode = rset.getString("Code");
+                String countryName = rset.getString("Name");
+                String continent = rset.getString("Continent");
+                String region = rset.getString("Region");
+                int population = rset.getInt("Population");
+                String capital = rset.getString("Capital");
+
+                Country country = new Country(countryCode, countryName, continent, region, population, capital);
+                countries.add(country);
             }
-            else
-                return null;
+
+        } catch (SQLException e) {
+            System.out.println("SQL Exception: " + e.getMessage());
         }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-            System.out.println("Failed to get employee details");
-            return null;
+        return countries;
+    }
+
+    // Function to print countries in a table format
+    public static void printCountries(List<Country> countries, String header) {
+        System.out.println("\n\n######## " + header + " ########");
+        System.out.println("+--------------+-----------------------------------------+---------------+-----------------------------+------------+-----------------------------------+");
+        System.out.printf("| %-12s | %-39s | %-13s | %-27s | %-10s | %-33s |\n",
+                "Country Code", "Country Name", "Continent", "Region", "Population", "Capital");
+        System.out.println("+--------------+-----------------------------------------+---------------+-----------------------------+------------+-----------------------------------+");
+
+        for (Country country : countries) {
+            System.out.printf("| %-12s | %-39s | %-13s | %-27s | %-10s | %-33s |\n",
+                    country.getCountryCode(), country.getName(), country.getContinent(),
+                    country.getRegion(), country.getPopulation(), country.getCapital());
+        }
+
+        System.out.println("+--------------+-----------------------------------------+---------------+-----------------------------+------------+-----------------------------------+");
+
+        // Check if countries were retrieved
+        if (countries.isEmpty()) {
+            System.out.println("No countries found.");
+        } else {
+            System.out.println(countries.size() + " countries found.");
         }
     }
 
-    public void displayEmployee(Employee emp)
-    {
-        if (emp != null)
-        {
-            System.out.println(
-                    emp.emp_no + " "
-                            + emp.first_name + " "
-                            + emp.last_name + "\n"
-                            + emp.title + "\n"
-                            + "Salary:" + emp.salary + "\n"
-                            + emp.dept_name + "\n"
-                            + "Manager: " + emp.manager + "\n");
-        }
+    public void Table_display(){
+        List<Country> countryByWorld = getPopulatedCountries(con, null, null, 0); // Fetch top 10 populated countries
+        printCountries(countryByWorld, "---------------------Most populated countries [World]---------------------");
+        List<Country> countryByContient = getPopulatedCountries(con, "Continent", "Europe", 0); // Fetch top 10 populated countries
+        printCountries(countryByContient, "---------------------Most populated countries [Continent] [Europe]---------------------");
+        List<Country> countryByRegion = getPopulatedCountries(con, "Region", "Southern and Central Asia", 0); // Fetch top 10 populated countries
+        printCountries(countryByRegion, "---------------------World most populated countries [Region] [Southern and Central Asia]---------------------");
+        List<Country> Top_10_ByWorld = getPopulatedCountries(con, null, null, 10); // Fetch top 10 populated countries
+        printCountries(Top_10_ByWorld, "---------------------Top 10 most populated countries [World]---------------------");
+        List<Country> Top_10_ByContient = getPopulatedCountries(con, "Continent", "North America", 10); // Fetch top 10 populated countries
+        printCountries(Top_10_ByContient, "---------------------Top 10 most populated countries [Continent][South ]---------------------");
+        List<Country> Top_10_ByRegion = getPopulatedCountries(con, "Region", "Caribbean", 10); // Fetch top 10 populated countries
+        printCountries(Top_10_ByRegion, "---------------------Top 10 most populated countries [Region][Caribbean]---------------------");
     }
 
-    public static void main(String[] args)
-    {
-        // Create new Application
-        App a = new App();
+    public static void main(String[] args) {
+        // Create new Application instance
+        App app = new App();
+
         // Connect to database
-        a.connect();
-//        // Get Employee
-//        Employee emp = a.getEmployee(255530);
-//        // Display results
-//        a.displayEmployee(emp);
-//        // Disconnect from database
-        a.disconnect();
+        app.connect();
+
+        // Set the limit to a positive number to retrieve results
+        app.Table_display();
+
+        app.disconnect();
     }
 }
